e286609ec31e280e627972f8cb0b975ee379937f
Robert Wolff <robert@thedeveco.com>
2025-11-27 12:27:40 -0800

    fix logo

--- src/components/ui/ClientLogos.vue
+++ src/components/ui/ClientLogos.vue
@@ -22,7 +22,7 @@
         <div class="client-logo">
           <img src="/clients/edgeai.png" alt="Edge AI Foundation" />
         </div>
-        <div class="public/clients/client-logo">
+        <div class="client-logo">
           <img src="/clients/solidrun.png" alt="SolidRun" />
         </div>
       </div>
